d8f4646b595902464a4227e22d624caac4d38465
Gyoza-Gyoza <136580712+Gyoza-Gyoza@users.noreply.github.com>
2026-07-06 14:03:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,20 +25,20 @@ def try_deploy(request: Request):
     else:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
-@app.post(
-    "/test",
-    status_code = status.HTTP_200_OK)
-def test(request: Request):
-    if authorize(request):
-        try:
-            path = Path("/home/deploy/chipin-api/test.sh")
-            logs = subprocess.run(["bash", path], check=True, capture_output=True, text=True)
-            print(logs.stdout)
-            return True
-        except subprocess.CalledProcessError as e:
-            raise HTTPException(status_code=e.returncode, detail=e.stderr)
-    else:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+# @app.post(
+#     "/test",
+#     status_code = status.HTTP_200_OK)
+# def test(request: Request):
+#     if authorize(request):
+#         try:
+#             path = Path("/home/deploy/chipin-api/test.sh")
+#             logs = subprocess.run(["bash", path], check=True, capture_output=True, text=True)
+#             print(logs.stdout)
+#             return True
+#         except subprocess.CalledProcessError as e:
+#             raise HTTPException(status_code=e.returncode, detail=e.stderr)
+#     else:
+#         raise HTTPException(status_code=401, detail="Unauthorized")
 
 def authorize(request: Request):
     auth_header = request.headers.get("Authorization")
